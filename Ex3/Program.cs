022164006b20294c2472272152f18bd21969bdd2
Nessa <la-nessa@mail.ru>
2022-12-11 00:02:56 +0300

    Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿void CubeArray (int [] arr, int len )
+{
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = i + 1;
+        int cube = arr[i] * arr[i] * arr[i];
+        Console.WriteLine(cube);
+    }
+}
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+
+CubeArray(array, N);
