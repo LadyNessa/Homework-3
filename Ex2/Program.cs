473150edbb1fc3791b3340d5d4f13a4ebaf20984
Nessa <la-nessa@mail.ru>
2022-12-11 01:32:21 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+Console.Write("Расстояние: ");
+Console.WriteLine(d);
